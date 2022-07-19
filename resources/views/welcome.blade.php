15a2773ed0e7f285d9584d53ba8fb7a07c45ad48
moeen1998 <58312233+moeen1998@users.noreply.github.com>
2022-07-19 15:56:19 -0700

    home link added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -179,7 +179,7 @@
                         <!-- Left Side Of Navbar -->
                         <ul class="navbar-nav me-auto" >
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">Home</a>
+                                <a class="nav-link" href="{{ url('/') }}">Home</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">Shop</a>
